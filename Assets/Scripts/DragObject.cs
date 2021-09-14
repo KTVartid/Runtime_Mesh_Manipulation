30389353c7a49d21d5794234ef612b097848d15d
KTVartid <kovacst@vartid.com>
2021-09-14 09:32:35 +0200

    repo created, v0.9

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class DragObject : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    private MeshStudy mesh;
+    private int Index;
+
+
+    public void Init(int Index, MeshStudy mesh)
+    {
+        this.mesh = mesh;
+        this.Index = Index;
+
+    }
+
+    private void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        // Store offset = gameobject world pos - mouse world pos
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        // pixel coordinates (x, y)
+        Vector3 mousePoint = Input.mousePosition;
+
+        // z coordinate of game object on screen
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+        mesh.DoAction(Index, transform.position);
+    }
+
+}
